e9f0c124a13b0e1cd4a2de3abd823f55c3068758
Han <84341984+Doyun-Han@users.noreply.github.com>
2023-11-30 21:42:33 +0900

    [Silver V] Title: 분수찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,10 +1,10 @@
 n = int(input())
 line = 1
 
-while n > line :
+while n > line : #분수는 사선 line의 갯수만큼 원소를 가지므로 n번째 라인에서 m번째를 구하기 위해 사용
     n -= line
     line += 1
-    
+
 if line % 2 == 1 :
     bottom = n
     top = line - n + 1
